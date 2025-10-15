38af835143e37449437f7a8fbf1c95d888d3c416
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-15 22:11:15 +0300

    Update genmap.py

--- dronepoint/genmap.py
+++ dronepoint/genmap.py
@@ -1,10 +1,11 @@
 import rospy
-from gazebo_msgs.srv import SpawnModel
-from geometry_msgs.msg import Pose, Point, Quaternion
-#from tf.transformations import quaternion_from_euler
+from gazebo_msgs.srv import SpawnModel, DeleteModel
+from geometry_msgs.msg import Pose, Point
 import random
+import sys
 
 spawn_service = rospy.ServiceProxy('/gazebo/spawn_sdf_model', SpawnModel)
+delete_service = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
 dronepoints = ["blue", "green", "yellow", "red"]
 
 class cube:
@@ -30,34 +31,51 @@ class cube:
                              robot_namespace='',
                              initial_pose=self.pose,
                              reference_frame="world")
-        print(gen.status_message, " ", self.color , " building" )
+        print(gen.status_message, " ", self.color , " building")
+
+    def delete(self):
+        resp = delete_service(self.color)
+        print(resp.status_message, " ", self.color , " building")
      
 
 
 def main():
-    x_list = []
-    y_list = []
-    free_x = [0,1,2,3,4,5,6,7,8,9]
-    free_y = [0,1,2,3,4,5,6,7,8,9]
+    if sys.argv[1] == "clear":
+        dronepoint_blue = cube(color="blue")
+        dronepoint_green = cube(color="green")
+        dronepoint_yellow = cube(color="yellow")
+        dronepoint_red = cube(color="red")
 
-    for x in range(4):
-        x = random.choice(free_x)
-        x_list.append(x)
-        free_x.remove(x)
-    for y in range(4):
-        y = random.choice(free_y)
-        y_list.append(y)
-        free_y.remove(y)
+        dronepoint_blue.delete()
+        dronepoint_green.delete()
+        dronepoint_yellow.delete()
+        dronepoint_red.delete()
+
+
+    else:
+        x_list = []
+        y_list = []
+        free_x = [0,1,2,3,4,5,6,7,8,9]
+        free_y = [0,1,2,3,4,5,6,7,8,9]
+
+        for x in range(4):
+            x = random.choice(free_x)
+            x_list.append(x)
+            free_x.remove(x)
+        for y in range(4):
+            y = random.choice(free_y)
+            y_list.append(y)
+            free_y.remove(y)
     
-    dronepoint_blue = cube(x_list[0], y_list[0], "blue")
-    dronepoint_green = cube(x_list[1], y_list[1], "green")
-    dronepoint_yellow = cube(x_list[2], y_list[2], "yellow")
-    dronepoint_red = cube(x_list[3], y_list[3], "red")
+        dronepoint_blue = cube(x_list[0], y_list[0], "blue")
+        dronepoint_green = cube(x_list[1], y_list[1], "green")
+        dronepoint_yellow = cube(x_list[2], y_list[2], "yellow")
+        dronepoint_red = cube(x_list[3], y_list[3], "red")
 
-    dronepoint_blue.spawn()
-    dronepoint_green.spawn()
-    dronepoint_yellow.spawn()
-    dronepoint_red.spawn()
+        dronepoint_blue.spawn()
+        dronepoint_green.spawn()
+        dronepoint_yellow.spawn()
+        dronepoint_red.spawn()
 
 
 if __name__ == '__main__':
