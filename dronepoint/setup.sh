d73d05908c40468ec3664c3a21f34e22b9ca5340
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-16 21:01:58 +0300

    Create setup.sh

---
+++ dronepoint/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Setup started"
+cd NTO_LR/dronepoint
+mv dronepoint_red dronepoint_green dronepoint_blue dronepoint_yellow /catkin_ws/src/sitl_gazebo/models/
+
+if [ $? -eq 0 ]; then
+    echo "Files have been successfully moved to /catkin_ws/src/sitl_gazebo/models/"
+else
+    echo "ERROR: $?"
+fi
+
+mv genmap.py /home/clover
+
+if [ $? -eq 0 ]; then
+    echo "genmap.py is ready"
+else
+    echo "ERROR: $?"
+fi
+
+cd
+rm -rf dronepoint
+
+if [ $? -eq 0 ]; then
+    echo "dronepoint directory has been deleted"
+else
+    echo "ERROR: $?"
+fi
+
+echo "Done"
