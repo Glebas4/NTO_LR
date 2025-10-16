be23b51935f5edd8278a6b3fd384ccacb30dd14b
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-16 22:11:19 +0300

    Create flight.py

---
+++ flight.py
@@ -0,0 +1,63 @@
+import rospy
+from clover import srv
+from std_srvs.srv import Trigger
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from std_msgs.msg import String
+import cv2 as cv
+import math
+
+
+bridge = CvBridge()
+get_telemetry = rospy.ServiceProxy('get_telemetry', srv.GetTelemetry)
+navigate = rospy.ServiceProxy('navigate', srv.Navigate)
+land = rospy.ServiceProxy('land', Trigger)
+pub = rospy.Publisher('buildings', String, queue_size=1)
+colors = {
+    "red"   : (()()),
+    "green" : (()()),
+    "blue"  : (()()),
+    "yellow": (()()) 
+}
+buildings = []
+
+
+def navigate_wait(x=0, y=0, z=0, yaw=float('nan'), speed=0.5, frame_id='aruco_map', auto_arm=False, tolerance=0.2):
+    navigate(x=x, y=y, z=z, yaw=yaw, speed=speed, frame_id=frame_id, auto_arm=auto_arm)
+
+    while not rospy.is_shutdown():
+        telem = get_telemetry(frame_id='navigate_target')
+        if math.sqrt(telem.x ** 2 + telem.y ** 2 + telem.z ** 2) < tolerance:
+            break
+        rospy.sleep(0.2)
+
+
+def scan():
+    img = bridge.imgmsg_to_cv2(rospy.wait_for_message('main_camera/image_raw', Image), 'bgr8')
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    for col, val in colors.items():
+        bin = cv.inRange(hsv, val[0], val[1])
+        count = cv.CountNonZero(bin)
+        if count>30:
+            return col
+        
+    return False
+
+def main():
+    navigate_wait(x=0, y=0, z=2, frame_id="body")
+    for y in range(10):
+        for x in range(10):
+            navigate_wait(x=x, y=y, z=2)
+            result = scan()
+            if result:
+                buildings.append((result, str(x), str(y)))
+            pub.publish(data=buildings)
+
+    navigate_wait(x=0, y=0, z=2, frame_id="aruco_map")
+    land()
+    print("done")
+
+
+if __name__ == '__main__':
+    rospy.init_node('flight')
+    main()
