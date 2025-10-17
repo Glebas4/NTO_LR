3e194004c58c38a6cda246081adfe020d7065656
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-17 18:01:03 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cd dronepoint/dronepoints
 mv dronepoint_red dronepoint_green dronepoint_blue dronepoint_yellow /home/clover/catkin_ws/src/sitl_gazebo/models
 
 if [ $? -eq 0 ]; then
-    echo "Files have been successfully moved to home/clover/catkin_ws/src/sitl_gazebo/models"
+    echo "Models have been successfully moved to home/clover/catkin_ws/src/sitl_gazebo/models"
 else
     echo "ERROR: $?"
 fi
