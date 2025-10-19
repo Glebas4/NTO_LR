8aa60ddcfbfc7a97ef80582a5f96b11a90db1578
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-19 18:00:51 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -45,7 +45,7 @@ def gen_points():
         x = round(random.uniform(0, 9), 1)
         y = round(random.uniform(0, 9), 1)
         cords = (x, y)
-        if all(math.sqrt((cord[0] - cords[0])**2 + cord[1] - cords[1]**2) >=1 for cord in points):
+        if all(math.sqrt((cord[0] - cords[0])**2 + (cord[1] - cords[1]**2)) >=1 for cord in points):
             points.append(cords)
 
         
