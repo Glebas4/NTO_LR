45019f8fad57811e03959b7b7e050c10a5a96d4c
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-19 18:14:35 +0300

    Update genmap.py

--- genmap.py
+++ genmap.py
@@ -20,7 +20,7 @@ class building:
 
         self.name = name
         self.color = color
-        self.path = "catkin_ws/src/sitl_gazebo/models/dronepoint_" + color + ".sdf"
+        self.path = "/home/clover/catkin_ws/src/sitl_gazebo/models/dronepoint_" + color + "/dronepoint_" + color + ".sdf"
 
     def spawn(self):
         with open(self.path, 'r') as f:
