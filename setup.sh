8d6e5cc20d1c76e13c86b4d2cf2975590b3da3da
Glebas4 <gleb.gurov.jr@gmail.com>
2025-10-19 18:41:21 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
 echo "Setup started"
-cd dronepoint/dronepoints
-mv dronepoint_red dronepoint_green dronepoint_blue dronepoint_yellow /home/clover/catkin_ws/src/sitl_gazebo/models
+rm /home/catkin_ws/src/clover/clover/launch/aruco.launch
+rm /home/catkin_ws/src/clover/clover/launch/clover.launch
 
+cd dronepoint/dronepoints
+
+mv dronepoint_red dronepoint_green dronepoint_blue dronepoint_yellow /home/clover/catkin_ws/src/sitl_gazebo/models
 if [ $? -eq 0 ]; then
     echo "Models have been successfully moved to home/clover/catkin_ws/src/sitl_gazebo/models"
 else
     echo "ERROR: $?"
 fi
+
+mv aruco.launch clover.launch /home/catkin_ws/src/clover/clover/launch
+if [ $? -eq 0 ]; then
+    echo "Launch files have been successfully configured"
+else
+    echo "ERROR: $?"
+fi
+
 echo "Done"
